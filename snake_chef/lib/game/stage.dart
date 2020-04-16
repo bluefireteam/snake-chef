f831864b5392b659dc1f0918322c986a554aee5f
Tyemy Kuga <tyemy.kuga@dextra-sw.com>
2020-04-15 23:00:28 -0300

    Loading stage from json

--- snake_chef/lib/game/stage.dart
+++ snake_chef/lib/game/stage.dart
@@ -6,15 +6,19 @@ class Stage {
   final List<Recipe> recipes;
   final List<Position> obstacles;
 
+  int time;
   int initialX;
   int initialY;
 
-  Stage({
-    this.recipes,
-    this.obstacles,
-    this.initialX,
-    this.initialY,
-  });
+  Stage({this.recipes, this.obstacles, this.initialX, this.initialY, this.time});
+
+  factory Stage.fromJson(Map<String, dynamic> json) => Stage(
+        time: json["time"],
+        recipes: json["recipes"].map((recipe) => Recipe.fromJson(recipe)).toList().cast<Recipe>(),
+        obstacles: json["obstacles"].map((obstacle) => Position(obstacle["x"], obstacle["y"])).toList().cast<Position>(),
+        initialX: json["initialX"],
+        initialY: json["initialY"],
+      );
 
   List<Ingredient> stageIngredients() {
     List<Ingredient> _ingredients = [];
@@ -36,6 +40,12 @@ class Recipe {
 
   Recipe({this.ingredients});
   Recipe.salad() : this(ingredients: [Ingredient.TOMATO, Ingredient.LETTUCE]);
+
+  factory Recipe.fromJson(Map<String, dynamic> json) {
+    final ingredients = json["ingredients"].map((ingredient) => Ingredient.values.firstWhere((e) => e.toString() == 'Ingredient.$ingredient'));
+
+    return Recipe(ingredients: ingredients.toList().cast<Ingredient>());
+  }
 
   bool checkCompletion(List<Ingredient> collectedIngredients) {
     return ingredients.every((ingredient) => collectedIngredients.contains(ingredient));
