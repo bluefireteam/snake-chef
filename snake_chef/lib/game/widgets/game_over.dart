c8913adc28e9be5ff30a7684c3afddca9d63f5c1
Erick Zanardo <erick.zanardo@dextra-sw.com>
2020-04-15 22:14:10 -0300

    Adding game over screen

--- snake_chef/lib/game/widgets/game_over.dart
+++ snake_chef/lib/game/widgets/game_over.dart
@@ -1,23 +1,37 @@
 import 'package:flutter/material.dart';
 
+import '../../widgets/button.dart';
+import '../../widgets/label.dart';
+
 class GameOver extends StatelessWidget {
   final VoidCallback restartGame;
 
   GameOver({this.restartGame});
 
   @override
-  Widget build(_) {
+  Widget build(ctx) {
     return Center(
       child: Container(
-          width: 100,
-          height: 100,
-          color: const Color(0xFFFF0000),
+          decoration: BoxDecoration(
+              border: Border.all(width: 10, color: Color(0xFFe3e3e3)),
+              color: const Color(0xFF566c86),
+          ),
+          width: 300,
+          height: 300,
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              Text('Paused'),
-              RaisedButton(
+              Label(label: 'Game Over', fontSize: 45, fontColor: Color(0xFF94b0c2)),
+              Button.primaryButton(
+                  label: 'Restart',
                   onPressed: restartGame,
-                  child: Text('Restart', style: TextStyle(fontSize: 20)))
+              ),
+              Button.secondaryButton(
+                  label: 'Back',
+                  onPressed: () {
+                    Navigator.of(ctx).pushReplacementNamed("/title");
+                  },
+              ),
             ],
           )),
     );
