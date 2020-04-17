cf3d760ab05b0cc00b2e9f141dd43db76e310153
Tyemy Kuga <tyemy.kuga@dextra-sw.com>
2020-04-16 23:09:38 -0300

    Center game board

--- snake_chef/lib/screens/game_screen.dart
+++ snake_chef/lib/screens/game_screen.dart
@@ -8,22 +8,24 @@ import '../audio_manager.dart';
 class GameScreen extends StatelessWidget {
   @override
   Widget build(ctx) {
-
     AudioManager.loopBackgroundMusic('gameplay.mp3');
 
     final stageNumber = ModalRoute.of(ctx).settings.arguments;
 
-    return Scaffold(
-      body: FutureBuilder(
-          future: StageLoader.loadStage(stageNumber),
-          builder: (BuildContext ctx, AsyncSnapshot<Stage> snapshot) {
-            if (snapshot.hasData) {
-              final Stage stage = snapshot.data;
+    return Container(
+        color: Colors.black,
+        child: Scaffold(
+          backgroundColor: Colors.black,
+          body: FutureBuilder(
+              future: StageLoader.loadStage(stageNumber),
+              builder: (BuildContext ctx, AsyncSnapshot<Stage> snapshot) {
+                if (snapshot.hasData) {
+                  final Stage stage = snapshot.data;
 
-              return Center(child: SnakeChef(boardWidth: 10, boardHeight: 10, stage: stage).widget);
-            }
-            return Container();
-          }),
-    );
+                  return Center(child: Container(width: 805, height: 620, child: SnakeChef(boardWidth: 10, boardHeight: 10, stage: stage).widget));
+                }
+                return Container();
+              }),
+        ));
   }
 }
