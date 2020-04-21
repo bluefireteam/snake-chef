1c1e586f662165a3640bc3ae1122f3a8209882b0
Erick Zanardo <erick.zanardo@dextra-sw.com>
2020-04-21 12:46:44 -0300

    Adding splash screen

--- snake_chef/lib/main.dart
+++ snake_chef/lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flame/flame.dart';
+import 'package:flame_splash_screen/flame_splash_screen.dart';
 
 import './screens/credits_screen.dart';
 import './screens/title_screen.dart';
 import './screens/game_screen.dart';
 import './screens/stage_select_screen.dart';
-import './screens/loading_screen.dart';
+
+import './audio_manager.dart';
+import './widgets/assets.dart';
+import './game/assets.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,16 +17,30 @@ void main() async {
   await Flame.util.fullScreen();
   await Flame.util.setLandscape();
 
+  Future.wait([
+    Assets.load(),
+    WidgetsAssets.load(),
+    AudioManager.load(),
+  ]);
+
   runApp(
       MaterialApp(
           routes: {
-            '/loading': (ctx) => LoadingScreen(),
+            '/': (ctx) => FlameSplashScreen(
+                theme: FlameSplashTheme.dark,
+                showBefore: (BuildContext context) {
+                  return Image.asset('assets/images/fireslime-banner.png',
+                      width: 400);
+                },
+                onFinish: (BuildContext context) {
+                  Navigator.pushNamed(context, "/title");
+                },
+            ),
             '/title': (ctx) => TitleScreen(),
             '/game': (ctx) => GameScreen(),
             '/stage_select': (ctx) => StageSelectScreen(),
             '/credits': (ctx) => CreditsScreen(),
           },
-          initialRoute: '/loading',
 
       )
   );
